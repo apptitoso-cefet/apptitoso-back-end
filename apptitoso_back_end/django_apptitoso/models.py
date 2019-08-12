691c816fac3434f5fd2f8d038ea3b01e429739b4
Unknown <pvitoreu@gmail.com>
2019-08-12 00:32:50 -0300

    Update model.py

--- apptitoso_back_end/django_apptitoso/models.py
+++ apptitoso_back_end/django_apptitoso/models.py
@@ -1,47 +1,53 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class ConceitoCulinario(models.Model):
-    nom_conceito = models.CharField(max_length=50)
-    foto_conceito = models.BinaryField(null=True)
+    nome = models.CharField(max_length=50)
+    foto = models.BinaryField(null=True)
 
 class Usuario(models.Model):
-    cod_email = models.CharField(max_length=100, unique=True)
-    des_senha = models.CharField(max_length=64)
-    nom_usuario = models.CharField(max_length=100)
-    foto_usuario = models.BinaryField()
+    email = models.CharField(max_length=100, unique=True)
+    senha = models.CharField(max_length=64)
+    nome = models.CharField(max_length=100)
+    foto = models.BinaryField()
     receitas_salvas = models.ManyToMany('Receita')
 
 class Receita(models.Model):
-    nom_receita = models.CharField(max_length=50)
-    des_receita = models.CharField()
-    foto_receita = models.BinaryField(null=True)
-    des_email = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField()
+    foto = models.BinaryField(null=True)
+    email = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     categorias = models.ManyToMany('Categoria')
 
 class Categoria(models.Model):
-    nom_categoria = models.CharField(max_length=20)
+    nome = models.CharField(max_length=20)
 
 class Passo(models.Model):
-    # PFK de receita
-    des_passo = models.CharField(max_length=20)
+    receita = models.ForeignKey('Receita', on_delete=models.CASCADE)
+    descricao = models.CharField(max_length=20)
     ordem_passo = models.IntegerField()
-    foto_passo = models.BinaryField()
+    foto = models.BinaryField(null=True)
+    timer = models.OneToOneField('PassoTimer', on_delete=models.CASCADE)
 
 class PassoTimer(models.Model):
-    # Falta chave
-    pass
+    tempo = models.TimeField()
 
 class Ingrediente(models.Model):
-    des_ingredientes = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=50)
 
-class UnidMedida(models.Model):
-    nom_unid_medida = models.CharField(max_length=50)
+class UnidadeDeMedida(models.Model):
+    nome = models.CharField(max_length=50)
 
 class ReceitaIngrediente():
-    pass
+    receita = models.ForeignKey('Receita', on_delete=models.CASCADE)
+    unidade_de_medida = models.ForeignKey('UnidadeDeMedida', on_delete=models.CASCADE)
+    quantidade = models.DecimalField(decimal_places=2, null=True)
 
 class AvaliacaoCriterio():
-    nom_criterio = models.CharField
+    nom_criterio = models.CharField(null=True)
 
 class Avaliacao():
-    pass
+    usuario = models.ForeignKey('Usuario')
+    receita = models.ForeignKey('Receita')
+    criterio_de_avaliacao = models.ForeignKey('AvaliacaoCriterio')
+    nota = models.PositiveSmallIntegerField(null=True)
